1d25838b3ea8394ca82ca6b8911f83aeb06324cd
priest-2105 <priest2105@gmail.com>
2025-03-08 05:43:02 +0100

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,13 @@ import type React from "react"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import favicon from "@/public/logo.png"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "moodmix",
   description: "Create and listen to mood-based playlists",
-    generator: 'v0.dev'
 }
 
 export default function RootLayout({
@@ -18,6 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="@/public/logo.png" type="image/x-icon" />
+        {/* <link rel="shortcut icon" href={favicon} type="image/x-icon" /> */}
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
